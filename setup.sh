76a063d510141b6f3b598c5b07380ce11ca50065
mjarosie <mjarosie@users.noreply.github.com>
2024-06-15 13:30:51 -0400

    add tmux config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Get the directory of the script (https://stackoverflow.com/a/56694491/3088888)
-DOTFILES_REPO_DIR_PATH=$(cd `dirname $0` && pwd)
+DOTFILES_REPO_DIR_PATH=$(cd $(dirname $0) && pwd)
 
 echo "Linking .vimrc"
 ln --symbolic "$DOTFILES_REPO_DIR_PATH/files/.vimrc" ~/.vimrc
 
 echo "Linking .gitconfig"
 ln --symbolic "$DOTFILES_REPO_DIR_PATH/files/.gitconfig" ~/.gitconfig
+
+echo "Linking .tmux.conf"
+ln --symbolic "$DOTFILES_REPO_DIR_PATH/files/.tmux.conf" ~/.tmux.conf
